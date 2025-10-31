0ce3ba3a097de71c33bce0cdee2e45d85277ea52
Renaitre <3414817426@qq.com>
2025-10-31 16:44:15 +0800

    update files

--- stable_diffusion/generate.py
+++ stable_diffusion/generate.py
@@ -1,9 +1,12 @@
 import torch
+import torchvision
 import os
 import random
 from PIL import Image
 from diffusers import AutoPipelineForText2Image, AutoPipelineForImage2Image
 from diffusers.utils import load_image
+import numpy as np
+from collections import Counter, defaultdict
 
 MODULE_STYLE = [
     "a user generated photo of",
@@ -43,6 +46,14 @@ GLOBAL_NEGATIVE_PROMPT = (
     "anime, logo, watermark, text, signature, unreal engine"
 )
 
+config = {
+    "base_model_id": "stabilityai/stable-diffusion-xl-base-1.0",
+    "refiner_model_id": "stabilityai/stable-diffusion-xl-refiner-1.0",
+    "ip_adapter_repo": "h94/IP-Adapter",
+    "ip_adapter_weights_dir": "sdxl_models",
+    "ip_adapter_weights_file": "ip-adapter-plus_sdxl_vit-h.safetensors"
+}
+
 def create_moduler_prompt(class_name):
     style = random.choice(MODULE_STYLE)
     context = random.choice(MODULE_CONTEXT)
@@ -52,4 +63,54 @@ def create_moduler_prompt(class_name):
 
     prompt = f"{style} {class_name_formatted}, {view}, {context}"
     return prompt
+
+def get_data(data_dir, lb_idx_path):
+    data_dir = os.path.join(data_dir, "food101")
+    dset = torchvision.datasets.Food101(data_dir, split="train", download=True)
+    img_files = dset._image_files                 # list[str]
+    labels_all = np.asarray(dset._labels, np.int64)  # np.ndarray[int]
+    classes = dset.classes                        # list[str]
+
+    lb_idx = np.asarray(np.load(lb_idx_path), np.int64).ravel()
+
+    class_to_data = defaultdict(list)
+    for i in lb_idx:
+        y = int(labels_all[i])
+        class_to_data[y].append(img_files[i])
+
+    if class_to_data:
+        n_max = max(len(v) for v in class_to_data.values())
+    else:
+        n_max = 0
+
+    class_to_gen = {classes[y]: (n_max - len(paths))
+                    for y, paths in class_to_data.items()}
+
+    return class_to_gen, class_to_data
+    
+def load_generation_pipeline(config, device="cuda"):
+    pipe = AutoPipelineForText2Image.from_pretrained(
+        config["base_model_id"],
+        torch_dtype=torch.dtype,
+        variant="bf16",
+        use_safetensors=True
+    )
+
+    refiner = AutoPipelineForImage2Image.from_pretrained(
+        config["refiner_model_id"],
+        text_encoder_2=pipe.text_encoder_2,
+        vae=pipe.vae,
+        torch_dtype=torch.dtype,
+        variant="bf16",
+        use_safetensors=True
+    )
+    pipe.refiner = refiner
+
+    pipe.load_ip_adapter(
+        config["ip_adapter_repo"],
+        subfolder=config["ip_adapter_weights_dir"],
+        weight_name=config["ip_adapter_weights_file"],
+    )
+    pipe.to(device)
+    return pipe
 
